4e3e0f7d8c448e545e40c719ac6b2aa3ed62c1f1
Suveesh <Suveesh@users.noreply.github.com>
2021-02-12 01:27:36 +0530

    Create process_data.py
    ETL pipeline
    created:  load and clean data

---
+++ data/process_data.py
@@ -0,0 +1,58 @@
+import sys
+import python as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv('messages_filepath')
+    categories = pd.read_csv('categories_filepath')
+    df = messages.merge(categories, on = 'id', left_index=True, right_index=True)
+    return df
+
+
+def clean_data(df):
+    categories = df['categories'].str.split(';', expand = True)
+    row = categories.iloc[0].str.split('-', expand = True)
+    categories.columns = list(row[0])
+    for column in categories:
+    # set each value to be the last character of the string
+    categories[column] = categories[column].str.split('-').str.get(-1)
+    # convert column from string to numeric
+    categories[column] = categories[column].astype(int)
+    df.drop(['categories'], axis = 1, inplace = True)
+    df = pd.concat([df, categories], axis = 1).drop_duplicates()
+    return df
+
+#def save_data(df, database_filename):
+    #pass  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+#if __name__ == '__main__':
+    #main()
